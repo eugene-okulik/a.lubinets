3dec4d47bb17ada657bf36545f0893ea3330b647
AndreiLubinets <a.lubinets92@gmail.com>
2024-11-18 14:43:28 +0300

    homework_5 done

---
+++ homework/andrei_lubinets/homework_5/task_1.py
@@ -0,0 +1,4 @@
+person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+name, last_name, city, phone, country = person
+
+print(f"name: {name}\nlast_name: {last_name}\ncity: {city}\nphone: {phone}\ncountry: {country}")
