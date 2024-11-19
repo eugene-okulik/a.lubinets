2763681e37b50260278d601d03774b1d5960a354
AndreiLubinets <a.lubinets92@gmail.com>
2024-11-19 06:08:22 +0300

    homework_5 fixed_task_2

--- homework/andrei_lubinets/homework_5/task_2.py
+++ homework/andrei_lubinets/homework_5/task_2.py
@@ -1,6 +1,6 @@
 res1 = "результат операции: 42"
 res2 = "результат операции: 514"
 res3 = "результат работы программы: 9"
-print(int(res1[res1.index('42'):]) + 10)
-print(int(res2[res2.index('514'):]) + 10)
-print(int(res3[res3.index('9'):]) + 10)
+print(int(res1[res1.index(':') + 1:]) + 10)
+print(int(res2[res2.index(':') + 1:]) + 10)
+print(int(res3[res3.index(':') + 1:]) + 10)
